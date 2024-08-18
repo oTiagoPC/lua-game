8b46236947387890ffec8ac4c7883b1998034a5c
Tiago Azevedo <tiagogadenz@icloud.com>
2024-08-17 23:26:11 -0300

    feat: added dash to the movement

--- main.lua
+++ main.lua
@@ -52,6 +52,13 @@ function love.load()
     
 end
 
+local dashSpeed = 5 -- velocidade do dash
+local dashTime = 0.2 -- tempo de duração do dash
+local dashCooldown = 0.5 -- tempo de cooldown do dash
+local canDash = true -- se pode dar dash
+local isDashing = false -- se está dando dash
+local dashDirection = {x = 0, y = 0} -- direção do dash
+
 function love.update(dt)
     local isMoving = false
     
@@ -78,6 +85,32 @@ function love.update(dt)
         isMoving = true
     end
     
+    -- Dash
+    if love.keyboard.isDown("lshift") and canDash then
+        isDashing = true
+        canDash = false
+        dashDirection.x, dashDirection.y = vx, vy
+    end
+
+    if isDashing then
+        vx, vy = dashDirection.x * dashSpeed, dashDirection.y * dashSpeed
+        dashTime = dashTime - dt -- dt é o tempo que passou desde o último frame
+        if dashTime <= 0 then
+            isDashing = false
+            dashTime = 0.2
+        end
+    end
+
+    -- Dash cooldown
+    if not canDash then
+        dashCooldown = dashCooldown - dt
+        if dashCooldown <= 0 then
+            canDash = true
+            dashCooldown = 1
+        end
+    end
+
+
     player.collider:setLinearVelocity(vx, vy)
     
     -- checa se player está se movendo
