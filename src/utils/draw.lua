55fdef3f84523c1da3cd8b0db60e18624daa9057
Tiago Pinheiro <tiagopinheiroc21@gmail.com>
2024-09-03 18:14:09 -0300

    fixed player collision

--- src/utils/draw.lua
+++ src/utils/draw.lua
@@ -2,7 +2,7 @@ function drawInCamera()
     gameMap:drawLayer(gameMap.layers["ground"])
     gameMap:drawLayer(gameMap.layers["objects"])
 
-    player.anim:draw(player.spritesheet, player.x, player.y-2, nil, player.dirX, 1, 9.5, 10.5)
+    player.anim:draw(player.spritesheet, player.x, player.y-2, nil, player.dirX, 1, 11, 10.5)
     if #world.enemies > 0 then
         for i, enemy in ipairs(world.enemies) do
             enemy.anim:draw(enemy.spritesheet, enemy.x, enemy.y, nil, 6, nil, 6, 9)
