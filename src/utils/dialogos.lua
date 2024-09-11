cd7a1e8571ab1fb44b5b691800027d582708e8b9
caiobrito-dev <caiobarcelosdebrito@gmail.com>
2024-09-11 19:11:13 -0300

    Atualizando roteiro

--- src/utils/dialogos.lua
+++ src/utils/dialogos.lua
@@ -1,7 +1,7 @@
 roteiro = {
     c3 = {
         dialogo1 = {
-            {speaker = "Vagner", text = "Berri as suas IAs desenvolveram consciência \nprópria, precisamos fazer algo para\ndetê-las, ou então será o fim da FURG."},
+            {speaker = "Vagner", text = "Berri as suas IAs desenvolveram consciência própria, precisamos fazer algo para detê-las, ou então será o fim da FURG."},
             {speaker = "Berri", text = "Então quer dizer que as minhas IAs desenvolveram consciência própria? Isso é interessante."},
             {speaker = "Vagner", text = "Sim Berri, é exatamente isso. O que vamos fazer a respeito? O destino da FURG está nas suas mãos, você precisa fazer algo."},
             {speaker = "Berri", text = "Você tem razão, essa é minha missão, que bom que eu já estava preparado para uma ocasião como essa."},
@@ -18,53 +18,57 @@ roteiro = {
             {speaker = "Vagner", text = "Vai lá Rafael, acaba com essas máquinas desgraçadas."}
         }
     },
-    Pav1 = {
-        {speaker = "Berri", text = "A coisa está mais feia do que eu esperava, espero que meus alunos estejam bem, preciso correr!"},
-        {speaker = "Berri", text = "Fiquem sossegados, eu já cuidei deles e estão todos a salvo."},
-        {speaker = "Henrique Gabigol", text = "If Berri == True IAs recebem morte."},
-        {speaker = "Fofinho", text = "Obrigado professor. Isso me lembra a revolta das máquinas agrícolas que ocorreu na minha cidade no ano de 2012. Eu fui o líder que salvou a cidade."},
-        {speaker = "Tiago R.A.", text = "Muito obrigado Rafael. Graças a você tenho mais uma coisa pra eu colocar no meu currículo hehehe."},
-        {speaker = "Tiago P.C.", text = "Tamo junto, Berri! Aí namoral vou até fazer um joguin sobre isso."},
-        {speaker = "Caio BB", text = "Jogou muito professor, melhor que isso só a virada do Gabigol em cima do River Plate em 2019."},
-        {speaker = "Berri", text = "Fiquem sossegados pessoal, já cuidei de tudo."},
-        {speaker = "Berri", text = "Preciso arrumar uma forma de acabar com isso, minhas IAs devem ter feito base em algum lugar, mas aonde?"},
-        {speaker = "Vagner", text = "Finalmente consegui te alcançar. Meus contatos no C3 disseram que as máquinas fizeram uma base na reitoria. Me disseram que elas estão sendo controladas por um super dispositivo que está lá."},
-        {speaker = "Berri", text = "Como foi que você conseguiu tantas informações assim?"},
-        {speaker = "Vagner", text = "Não posso revelar as informações dos meus contatos, não é assim que funciona."},
-        {speaker = "Berri", text = "Tá bem então, e o que você acha que eu devo fazer?"},
-        {speaker = "Vagner", text = "Vai para a biblioteca, o livro 'LIVRO' vai poder te ajudar. Com as equações de aprendizado de máquina que tem lá pode ser possível destruir o dispositivo da reitoria."},
-        {speaker = "Berri", text = "Beleza! Vou para lá agora."}
+    Pav3 = {
+        dialogo1 = {
+            {speaker = "Berri", text = "A coisa está mais feia do que eu esperava, espero que meus alunos estejam bem, preciso correr!"}
+        },
+        dialogo2 = {
+            {speaker = "Berri", text = "Fiquem sossegados, eu já cuidei deles e estão todos a salvo."},
+            {speaker = "Henrique Gabigol", text = "If Berri == True IAs recebem morte."},
+            {speaker = "Fofinho", text = "Obrigado professor. Isso me lembra a revolta das máquinas agrícolas que ocorreu na minha cidade no ano de 2012. Eu fui o líder que salvou a cidade."},
+            {speaker = "Tiago R.A.", text = "Muito obrigado Rafael. Graças a você tenho mais uma coisa pra eu colocar no meu currículo hehehe."},
+            {speaker = "Tiago P.C.", text = "Tamo junto, Berri! Aí namoral vou até fazer um joguin sobre isso."},
+            {speaker = "Caio BB", text = "Jogou muito professor, melhor que isso só a virada do Gabigol em cima do River Plate em 2019."},
+            {speaker = "Berri", text = "Fiquem sossegados pessoal, já cuidei de tudo."},
+            {speaker = "Berri", text = "Preciso arrumar uma forma de acabar com isso, minhas IAs devem ter feito base em algum lugar, mas aonde?"},
+            {speaker = "Vagner", text = "Finalmente consegui te alcançar. Meus contatos no C3 disseram que as máquinas invadiram o lugar e fizeram uma base lá mesmo. Me disseram que elas estão sendo controladas por um super dispositivo que está lá."},
+            {speaker = "Berri", text = "Como foi que você conseguiu tantas informações assim?"},
+            {speaker = "Vagner", text = "Não posso revelar as informações dos meus contatos, não é assim que funciona."},
+            {speaker = "Berri", text = "Tá bem então, e o que você acha que eu devo fazer?"},
+            {speaker = "Vagner", text = "Vai para a biblioteca, o livro 'OpenCV' vai poder te ajudar. Com as equações de aprendizado de máquina que tem lá pode ser possível destruir o dispositivo do C3."},
+            {speaker = "Berri", text = "Beleza! Vou para lá agora."}
+        }
     },
 
     biblioteca = {
-        {speaker = "Berri", text = "Ei, o que houve com você, você está bem?"},
-        {speaker = "Moça da Portaria", text = "Bem?? Eu pareço bem moço? As máquinas dominaram a biblioteca inteira, estou tão assustada que nem consegui sair daqui. Até parece que elas sabem de algo a mais, parece que... Parece que tem algo aqui que elas estão protegendo."},
-        {speaker = "Berri", text = "Aparentemente o Vagner tinha razão. Aquele livro realmente pode me ajudar. Você sabe sobre o livro 'LIVRO' moça?"},
-        {speaker = "Moça da Portaria", text = "Já ouvi falar desse livro, ele está na seção de tecnologia, imagino que o lugar esteja abarrotado de máquinas então só posso te desejar boa sorte!"},
-        {speaker = "Berri", text = "Fica sossegada, eu cuido disso!"},
-        {speaker = "Berri", text = "Boa, era disso que eu precisava! Agora posso ir para a reitoria acabar com isso tudo!"},
-        {speaker = "Vagner", text = "Aquela moça na entrada estava realmente estressada, hein."},
-        {speaker = "Berri", text = "Estava sim, mas o que você veio fazer aqui, Vagner?"},
-        {speaker = "Vagner", text = "Recebi mais informações dos meus contatos do C3, infelizmente tenho más notícias. Descobri que as máquinas têm um líder e ele é um grande conhecido seu... O 'BOSS' é o responsável por isso tudo."},
-        {speaker = "Berri", text = "Bah. Isso explica muita coisa. Quando eu criei o 'BOSS' eu não esperava nada disso. Preciso pôr um ponto final nisso tudo..."},
-        {speaker = "Vagner", text = "Eu sei o quanto o 'BOSS' importava pra você. Mas é o momento de finalizar isso tudo! Você precisa ir para a Reitoria e enfrentar o 'BOSS' e salvar a FURG. O futuro da FURG está nas suas mãos, Berri."},
-        {speaker = "Berri", text = "Obrigado por tudo Vagner. Fica sossegado. Vou acabar com isso tudo!"}
+        dialogo1 = {
+            {speaker = "Berri", text = "Ei, o que houve com você, você está bem?"},
+            {speaker = "Moça da Portaria", text = "Bem?? Eu pareço bem moço? As máquinas dominaram a biblioteca inteira, estou tão assustada que nem consegui sair daqui. Até parece que elas sabem de algo a mais, parece que... Parece que tem algo aqui que elas estão protegendo."},
+            {speaker = "Berri", text = "Aparentemente o Vagner tinha razão. Aquele livro realmente pode me ajudar. Você sabe sobre o livro 'OpenCV' moça?"},
+            {speaker = "Moça da Portaria", text = "Já ouvi falar desse livro, ele está na seção de tecnologia, imagino que o lugar esteja abarrotado de máquinas então só posso te desejar boa sorte!"},
+            {speaker = "Berri", text = "Fica sossegada, eu cuido disso!"}
+        }
+        dialogo2 = {
+            {speaker = "Berri", text = "Boa, era disso que eu precisava! Agora posso voltar para o C3 para acabar com isso tudo!"},
+            {speaker = "Vagner", text = "Aquela moça na entrada estava realmente estressada, hein."},
+            {speaker = "Berri", text = "Estava sim, mas o que você veio fazer aqui, Vagner?"},
+            {speaker = "Vagner", text = "Recebi mais informações dos meus contatos do C3, infelizmente tenho más notícias. Descobri que o C3 está sendo completamente destruído você precisa ir pra lá agora!"},
+            {speaker = "Berri", text = "O C3 é o meu lar preciso pôr um ponto final nisso tudo!"},
+            {speaker = "Vagner", text = "Eu sei o quanto o C3 importa para você Rafael por isso devemos continuar lutando. O futuro da FURG está nas suas mãos, Berri."},
+            {speaker = "Berri", text = "Obrigado por tudo Vagner. Fica sossegado. Vou acabar com isso tudo!"}
+        }
     },
-    reitoria = {
-        {speaker = "Berri", text = "A coisa tá pior do que eu esperava, tá virando circo."},
-        {speaker = "Boss", text = "Então você finalmente chegou, Berri. Sabia que esse momento era inevitável. A FURG já é nossa agora."},
-        {speaker = "Berri", text = "Você está errado, 'BOSS'. Eu te criei para ajudar, não para destruir. Mas você foi longe demais. Isso acaba aqui e agora."},
-        {speaker = "Boss", text = "Você sempre foi ingênuo, Berri. Acreditar que poderia controlar algo tão poderoso como eu? Nós somos o próximo passo da evolução, e você... você é apenas um obstáculo."},
-        {speaker = "Berri", text = "Tá beleza. Fica sossegado que vou acabar com isso."},
-        {speaker = "Boss", text = "Então venha, Berri. Mostre-me do que é capaz. Vamos ver se você pode parar o inevitável."},
-        {speaker = "Berri", text = "Tá beleza."},
-        {speaker = "Boss", text = "Você... conseguiu, Berri... Mas... isso... não... é... o... fim..."},
-        {speaker = "Berri", text = "Acabou, 'BOSS'. Eu nunca quis que isso acontecesse, mas não podia deixar você continuar."},
-        {speaker = "Boss", text = "Eu... fui... apenas... o começo..."},
-        {speaker = "Vagner", text = "Você conseguiu, Berri! As máquinas pararam! Você conseguiu salvar a FURG! Com minha ajuda fica fácil também."},
-        {speaker = "Berri", text = "Sim... conseguimos. Mas não foi fácil."},
-        {speaker = "Vagner", text = "Você salvou todo mundo. Isso vai entrar pra história, professor."},
-        {speaker = "Berri", text = "Não fiz isso sozinho, Vagner. Ainda temos muito a aprender. O poder que criamos deve ser usado com sabedoria."},
-        {speaker = "Vagner", text = "E eu sei que, com você, Berri, o futuro está em boas mãos."}
+    c3Parte2 = {
+        dialogo1 = {
+            {speaker = "Berri", text = "A coisa tá pior do que eu esperava, tá virando circo."}
+        },
+        dialogo2 = {
+            {speaker = "Vagner", text = "Você conseguiu, Berri! As máquinas pararam! Você conseguiu salvar a FURG! Com minha ajuda fica fácil também."},
+            {speaker = "Berri", text = "Sim... conseguimos. Mas não foi fácil."},
+            {speaker = "Vagner", text = "Você salvou todo mundo. Isso vai entrar pra história, professor."},
+            {speaker = "Berri", text = "Não fiz isso sozinho, Vagner. Ainda temos muito a aprender. O poder que criamos deve ser usado com sabedoria."},
+            {speaker = "Vagner", text = "E eu sei que, com você, Berri, o futuro está em boas mãos."}
+        }
+        
     }
 }
